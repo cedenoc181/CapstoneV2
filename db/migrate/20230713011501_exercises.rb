9e312373c74ad1ac33f28ebf7dbee39fd652af85
Christian <cedenoc181@gmail.com>
2023-07-12 22:14:40 -0400

    added review migartion

--- db/migrate/20230713011501_exercises.rb
+++ db/migrate/20230713011501_exercises.rb
@@ -1,4 +1,11 @@
 class Exercises < ActiveRecord::Migration[7.0]
   def change
+    create_table :exercises do |t|
+      t.string :name 
+      t.string :target
+      t.string :bodyPart
+      t.string :equipment
+      t.string :gifUrl
+      t.timestamps
   end
 end
