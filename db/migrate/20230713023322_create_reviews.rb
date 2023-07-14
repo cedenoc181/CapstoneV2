c37a0e2a47c5dc75acb9b89661bc6389bd901bed
Christian <cedenoc181@gmail.com>
2023-07-14 13:33:36 -0400

    added columnns to review migration and idea suedo coded

--- db/migrate/20230713023322_create_reviews.rb
+++ db/migrate/20230713023322_create_reviews.rb
@@ -6,8 +6,12 @@ class CreateReviews < ActiveRecord::Migration[7.0]
       t.string :review_header
       t.text :users_review
       t.string :photo
-      t.string :star_rating 
+      t.string :star_rating_value
+      t.string :stars
       t.timestamps
     end
   end
 end
+# 5 onclicks or one if can make dry code, that will determine selected star rating then follow by if else or can make if else on the onclick to combine into one 
+# star image is gonna be generated with a if else statement on the front end, going to have 5 images of star ratings that will be selected by user 
+# the selected rating image url or star emojis  will be posted via fetch to represent star rating (make sure image is transparent)
