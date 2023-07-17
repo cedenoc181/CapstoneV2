40de574559d001c02cc611a84d1d8f543685b5f6
Christian <cedenoc181@gmail.com>
2023-07-16 22:00:15 -0400

    added pt relationships

--- app/models/physical_therapist.rb
+++ app/models/physical_therapist.rb
@@ -1,2 +1,6 @@
 class PhysicalTherapist < ApplicationRecord
+    has_many :appointments, dependent: :destroy
+    has_many :users, through: :appointments 
+    has_many :reviews, dependent: :destroy
+    has_many :users, through: :reviews
 end
