e99a91b89fabf476e599b41d76310bfd38db2311
Christian <cedenoc181@gmail.com>
2023-07-17 23:21:11 -0400

    added users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :email, :insurance, :first_name, :last_name, :state, :city, :address, :phone_number, :DOB, :appointments, :physical_therapists, :exercises, :reviews, :favorites
 end
