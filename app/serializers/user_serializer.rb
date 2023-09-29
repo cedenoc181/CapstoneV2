f415174a7b7c296d2c70d45cb9aa591d6e7c9631
Christian <cedenoc181@gmail.com>
2023-09-29 12:57:31 -0400

    added insurance member id to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :insurance, :first_name, :last_name, :state, :city, :address, :phone_number, :DOB, :appointments, :physical_therapists, :exercises, :reviews, :favorites
+  attributes :id, :email, :insurance, :insurance_member_id, :first_name, :last_name, :state, :city, :address, :phone_number, :DOB, :appointments, :physical_therapists, :exercises, :reviews, :favorites
 end
