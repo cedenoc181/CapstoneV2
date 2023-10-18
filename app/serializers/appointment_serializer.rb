0ff8569b889e9b2c3fc5e99b37c2dc706c1f0e76
Christian <cedenoc181@gmail.com>
2023-10-17 23:21:42 -0400

    finalized appointments

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,3 +1,5 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :physical_therapist_id, :case, :home_visit, :telemedicine, :scheduled, :user, :physical_therapist  
+  attributes :id, :user_id, :physical_therapist_id, :case, :home_visit, :telemedicine, :scheduled
+  belongs_to :user
+  belongs_to :physical_therapist  
 end
