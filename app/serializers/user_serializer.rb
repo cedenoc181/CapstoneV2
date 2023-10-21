db19339392cb958c06cdf96f28b625b376e947ae
Christian <cedenoc181@gmail.com>
2023-10-21 13:35:52 -0400

    made API for PT render user review in feedback attribute

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   # add expressions to serializer 
-  attributes :id, :email, :first_name, :last_name, :admin, :users_info
+  attributes :id, :email, :first_name, :last_name, :admin, :users_info, :reviews
   # has_one :users_info
-  has_many :reviews
+  # has_many :reviews
   has_many :appointments
   has_many :exercises
   has_many :providers_user_reviewed
