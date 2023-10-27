e76132c78f25db1e5450d82b451fcdf778680aaa
Christian <cedenoc181@gmail.com>
2023-10-27 06:32:54 -0400

    article controller updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authorized
   # skip before action :authorized allows me to index and show articles without a user logged in 
   skip_before_action :authorized, only: %i[show index]
   # before action runs set_article before having access to show update destroy
@@ -17,13 +18,13 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    @article = Article.create!(create_article_params)
+    @article = Article.create!(article_params)
     render json: @article, status: :created
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-      @article.update(update_article_params)
+      @article.update!(article_params)
       render json: @article
   end
 
@@ -40,12 +41,8 @@ class ArticlesController < ApplicationController
     end
 
     # Only allow a list of trusted parameters through.
-    def create_article_params
+    def article_params
       params.require(:article).permit(:physical_therapist_id, :title, :author, :article_body, :category, :published, :photo)
-    end
-
-    def update_article_params
-      params.require(:article).permit(:title, :author, :article_body, :category, :published, :photo)
     end
 
 end
