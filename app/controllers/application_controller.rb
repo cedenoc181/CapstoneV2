61b63fc6310729fd53db74b8e71c8b4812cd3eec
Christian <cedenoc181@gmail.com>
2023-10-26 22:28:47 -0400

    removed redundance for the appoinntment controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::API
     end
 
     private 
-    def render_record_not_found
+     def render_record_not_found
         render json: { error: 'Record not found' }, status: :not_found
       end
       
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,38 +1,36 @@
 class AppointmentsController < ApplicationController
-    skip_before_action :authorized, only: [:create, :index, :show, :destroy]
+    before_action :authorized
+    before_action :find_appointment, only: %i[show update destroy]
 
 
     def index
-        appointment = Appointment.all
-        render json: appointment, status: :ok
+        @appointment = Appointment.all
+        render json:  @appointment, status: :ok
     end
 
     def show
-        appointment = find_appointment
-        render json: appointment
+        render json:  @appointment
     end
 
     def update 
-        appointment = find_appointment
-        appointment.update!(update_appointments_params)
-       render json: appointment
+        @appointment.update!(update_appointments_params)
+       render json:  @appointment
       end 
 
     def create
-        appointment = Appointment.create!(create_appointment_params)
-        render json: appointment
+        @appointment = Appointment.create!(create_appointment_params)
+        render json:  @appointment
     end
 
     def destroy
-        appointment = find_appointment
-        appointment.destroy
+        @appointment.destroy
         head :no_content 
     end
 
     private 
 
     def find_appointment 
-        appointment = Appointment.find(params[:id])
+        @appointment = Appointment.find(params[:id])
     end
 
     def update_appointments_params
@@ -42,9 +40,5 @@ class AppointmentsController < ApplicationController
     def create_appointment_params
         params.permit(:user_id, :physical_therapist_id, :scheduled, :case, :telemedicine, :home_visit)
     end
-
-    def render_record_not_found 
-        render json: { error: "Appointment not found" }, status: :not_found 
-    end 
     
 end
