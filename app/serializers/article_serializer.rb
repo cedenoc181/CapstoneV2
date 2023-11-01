4e36f6d272adc9c278275f47a11751294c2378c6
Christian <cedenoc181@gmail.com>
2023-11-01 06:52:24 -0400

    added serializer relationship to articles

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,3 +1,6 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :article_body, :category, :published, :photo
+
+  belongs_to :physical_therapit
+  has_many :expressions
 end
