4c3d4899bda52c39f402195fcdf0389ee98b34c2
Christian <cedenoc181@gmail.com>
2023-11-01 06:58:14 -0400

    fixed type in article serializer and model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    belongs_to :physical_therapit
+    belongs_to :physical_therapist
     has_many :expressions, dependent: :destroy
 end
--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,6 +1,6 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :article_body, :category, :published, :photo
 
-  belongs_to :physical_therapit
+  belongs_to :physical_therapist
   has_many :expressions
 end
